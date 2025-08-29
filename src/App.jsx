8bce21fd4da8ade8e89a24464be59cf9251b132d
MielmaDev <elizabethmaranon80@gmail.com>
2025-08-29 17:32:15 +0200

    Elimina rutas y componentes no utilizados en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.scss';
 import Cabecera from './componentes/encabezado/Cabecera';
 import SobreMi from './componentes/paginas/SobreMi';
-import Proyectos from './componentes/paginas/Proyectos';
-import Contacto from './componentes/paginas/Contacto';
 import Pie from './componentes/pie/Pie'
 
 function App() {
@@ -14,13 +12,9 @@ function App() {
       <main className='contenido'>
         <h1>Bienvenido a mi portfolio</h1>
         <p>Este es un espacio para mostrar mis proyectos y habilidades.</p>
-        <Link to="/inicio">Inicio</Link>
       </main>
       <Routes>
-        <Route path="/inicio" element={<Inicio />} />
         <Route path="/sobre-mi" element={<SobreMi />} />
-        <Route path="/proyectos" element={<Proyectos />} />
-        <Route path="/contacto" element={<Contacto />} />
       </Routes>
       <Pie />
     </BrowserRouter>
